abd870e7d338a32f50b9b972e24954ff38cf0766
ddiniz <ddiniz@student.42sp.org.br>
2023-02-08 08:58:34 -0300

    Feat: add push operation prototypes.

---
+++ sources/rotate_rev_sametime.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_rev_sametime.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 17:59:45 by ddiniz            #+#    #+#             */
+/*   Updated: 2022/11/29 19:21:53 by ddiniz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <push_swap.h>
+
+int	rotate_rev_all(t_list *stack_source, t_list *stack_target)
+{
+	// TODO: rrr - reverse rotate a and b at the same time
+}
---
+++ sources/rotate_sametime.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rotate_sametime.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/29 17:58:42 by ddiniz            #+#    #+#             */
+/*   Updated: 2022/11/29 19:22:08 by ddiniz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <push_swap.h>
+
+int	rotate_all(t_list *stack_source, t_list *stack_target)
+{
+	// TODO: rr - rotate a and b at the same time
+}
