b7261b9aad7f2a9173014ef659c48f4d98ee98f7
Diego Diniz <ddiniz@student.42sp.org.br>
2023-03-01 19:22:27 -0500

    fix: solve argc argv mem issue.

--- sources/load_args.c
+++ sources/load_args.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 19:46:04 by ddiniz            #+#    #+#             */
-/*   Updated: 2023/02/28 21:56:20 by ddiniz           ###   ########.fr       */
+/*   Updated: 2023/03/01 19:14:40 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,18 @@ int	load_args(int max, char *vec[])
 {
 	t_cell	aux[max];
 	t_list	*top_stack_a;
+	int		i;
 
 	top_stack_a = NULL;
 
-	aux[0].value = ft_atoi(vec[1]);
-	aux[0].index = 1;
-	ft_lstadd_back(&top_stack_a, ft_lstnew(&aux[0].value));
-
-	aux[1].value = ft_atoi(vec[2]);
-	aux[1].index = 1;
-	ft_lstadd_back(&top_stack_a, ft_lstnew(&aux[1].value));
+	i = 0;
+	while (i < max - 1)
+	{
+		aux[i].value = ft_atoi(vec[i + 1]);
+		aux[i].index = i + 1;
+		ft_lstadd_back(&top_stack_a, ft_lstnew(&aux[i].value));
+		i++;
+	}
 
 	ft_printf("Stack A: %c\n", 0);
 	ft_lstiter(top_stack_a, &print_node);
--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 17:13:23 by ddiniz            #+#    #+#             */
-/*   Updated: 2023/02/28 21:05:00 by ddiniz           ###   ########.fr       */
+/*   Updated: 2023/03/01 19:20:25 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int argc, char *argv[])
 {
-	if (argc == 0)
+	if (argc <= 1)
 		return (EXIT_SUCCESS);
 	load_args(argc, argv);
 	return (EXIT_SUCCESS);
