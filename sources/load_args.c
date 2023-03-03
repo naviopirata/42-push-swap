ccc88fc10c2b06ea17fb3d6afe20a92a6ac56ded
Diego Diniz <ddiniz@student.42sp.org.br>
2023-03-02 22:35:09 -0500

    Refac: delimit responsability

--- sources/load_args.c
+++ sources/load_args.c
@@ -6,32 +6,28 @@
 /*   By: ddiniz <ddiniz@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 19:46:04 by ddiniz            #+#    #+#             */
-/*   Updated: 2023/03/01 19:14:40 by ddiniz           ###   ########.fr       */
+/*   Updated: 2023/03/02 22:25:31 by ddiniz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-int	load_args(int max, char *vec[])
+t_list	*load_args(int max, char *vec[])
 {
-	t_cell	aux[max];
-	t_list	*top_stack_a;
+	t_cell	*aux[max];
+	t_list	*list;
 	int		i;
 
-	top_stack_a = NULL;
+	list = NULL;
 
 	i = 0;
 	while (i < max - 1)
 	{
-		aux[i].value = ft_atoi(vec[i + 1]);
-		aux[i].index = i + 1;
-		ft_lstadd_back(&top_stack_a, ft_lstnew(&aux[i].value));
+		aux[i] = (t_cell *)malloc(sizeof(t_cell));
+		aux[i]->value = ft_atoi(vec[i + 1]);
+		aux[i]->index = i + 1;
+		ft_lstadd_back(&list, ft_lstnew(&aux[i]->value));
 		i++;
 	}
-
-	ft_printf("Stack A: %c\n", 0);
-	ft_lstiter(top_stack_a, &print_node);
-
-	ft_lstclear(&top_stack_a, &del);
-	return (EXIT_SUCCESS);
+	return (list);
 }
